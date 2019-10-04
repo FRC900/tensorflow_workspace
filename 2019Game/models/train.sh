2615cd16be236d0de20ae1501a0f96604ff12f32
Kevin Jaget <kjaget@nvidia.com>
2019-10-04 16:33:18 -0400

    Increased training max_steps

--- 2019Game/models/train.sh
+++ 2019Game/models/train.sh
@@ -3,7 +3,7 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/targets/x86_64-linux/lib/s
 PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2019Game/models/model/ssd_mobilenet_v2_coco.config
 #PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2019Game/models/model/ssd_mobilenet_v2_fpnlite_quantized_shared_box_predictor_256x256_depthmultiplier_75_coco14_sync.config
 MODEL_DIR=/home/ubuntu/tensorflow_workspace/2019Game/models
-NUM_TRAIN_STEPS=250000
+NUM_TRAIN_STEPS=350000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 python /home/ubuntu/models/research/object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
