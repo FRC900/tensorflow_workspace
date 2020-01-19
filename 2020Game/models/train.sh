cc4cb4189518735b7a696aed9861c049cf6196f6
Kevin Jaget <kjaget@nvidia.com>
2020-01-19 11:19:59 -0500

    Updates from debugging training runs

--- 2020Game/models/train.sh
+++ 2020Game/models/train.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Need pretrained model info for transfer learning setup
+# if [ ! -f ssd_mobilenet_v2_coco_2018_03_29/checkpoint ]
+# cd /home/ubuntu/tensorflow_workspace/2020Game/models
+# wget ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+# tar -xzf ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+# rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+# fi
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/targets/x86_64-linux/lib/stubs
 PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_coco.config
 MODEL_DIR=/home/ubuntu/tensorflow_workspace/2020Game/models
