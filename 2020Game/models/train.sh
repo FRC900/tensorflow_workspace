31f710714e4e8cf4527d54e00a573349b4a6bf4e
Team 900 Programmers <programmers@team900.org>
2020-01-20 15:16:20 -0500

    Fix wget path for pretrained mobilenet v2 ssd coco model

--- 2020Game/models/train.sh
+++ 2020Game/models/train.sh
@@ -2,7 +2,7 @@
 # Need pretrained model info for transfer learning setup
 # if [ ! -f ssd_mobilenet_v2_coco_2018_03_29/checkpoint ]
 # cd /home/ubuntu/tensorflow_workspace/2020Game/models
-# wget ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+# wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # tar -xzf ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # fi
