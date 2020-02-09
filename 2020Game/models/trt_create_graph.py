43d11ddc6e0d7721478be2c33f3da301f318f26d
Kevin Jaget <kjaget@nc.rr.com>
2020-02-09 17:32:00 -0500

    Initial work on porting to run using tensor-RT (#12)
    * Initial work on porting to run using tensor-RT
    
    * Update to newest model

---
+++ 2020Game/models/trt_create_graph.py
@@ -0,0 +1,139 @@
+'''
+Script to generate TRT output graph files from a given checkpoint
+
+TensorRT generates optimized code for running networks on GPUs, both for
+desktop and especially for the Jetson.  The optimization process takes a bit
+of time, so it is possible to preprocess and save the output. 
+That's the goal of this script.
+
+Edit the SAVED_MODEL_DIR and CHECKPOINT_NUMBER variables below and run it.
+'''
+import tensorflow as tf
+import tensorflow.contrib.tensorrt as trt
+from object_detection.protos import pipeline_pb2
+from object_detection import exporter
+from google.protobuf import text_format
+import os
+import subprocess
+
+from graph_utils import force_nms_cpu as f_force_nms_cpu
+from graph_utils import replace_relu6 as f_replace_relu6
+from graph_utils import remove_assert as f_remove_assert
+
+# Output file name
+TRT_OUTPUT_GRAPH = 'trt_graph.pb'
+
+# Dir where model.ckpt* files are being generated
+SAVED_MODEL_DIR='/home/ubuntu/tensorflow_workspace/2020Game/models/tmp4'
+CHECKPOINT_NUMBER='167973'
+
+# Network config
+CONFIG_FILE = '/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_coco.config'
+
+INPUT_NAME='image_tensor'
+BOXES_NAME='detection_boxes'
+CLASSES_NAME='detection_classes'
+SCORES_NAME='detection_scores'
+MASKS_NAME='detection_masks'
+NUM_DETECTIONS_NAME='num_detections'
+MODEL_CHECKPOINT_PREFIX='model.ckpt-'
+FROZEN_GRAPH_NAME='frozen_inference_graph.pb'
+
+# from tf_trt models dir
+def build_detection_graph(config, checkpoint,
+        batch_size=1,
+        score_threshold=None,
+        force_nms_cpu=True,
+        replace_relu6=True,
+        remove_assert=True,
+        input_shape=None,
+        output_dir='.generated_model'):
+    """Builds a frozen graph for a pre-trained object detection model"""
+    
+    config_path = config
+    checkpoint_path = checkpoint
+
+    # parse config from file
+    config = pipeline_pb2.TrainEvalPipelineConfig()
+    with open(config_path, 'r') as f:
+        text_format.Merge(f.read(), config, allow_unknown_extension=True)
+
+    # override some config parameters
+    if config.model.HasField('ssd'):
+        config.model.ssd.feature_extractor.override_base_feature_extractor_hyperparams = True
+        if score_threshold is not None:
+            config.model.ssd.post_processing.batch_non_max_suppression.score_threshold = score_threshold    
+        if input_shape is not None:
+            config.model.ssd.image_resizer.fixed_shape_resizer.height = input_shape[0]
+            config.model.ssd.image_resizer.fixed_shape_resizer.width = input_shape[1]
+    elif config.model.HasField('faster_rcnn'):
+        if score_threshold is not None:
+            config.model.faster_rcnn.second_stage_post_processing.score_threshold = score_threshold
+        if input_shape is not None:
+            config.model.faster_rcnn.image_resizer.fixed_shape_resizer.height = input_shape[0]
+            config.model.faster_rcnn.image_resizer.fixed_shape_resizer.width = input_shape[1]
+
+    if os.path.isdir(output_dir):
+        subprocess.call(['rm', '-rf', output_dir])
+
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+
+    # export inference graph to file (initial)
+    with tf.Session(config=tf_config) as tf_sess:
+        with tf.Graph().as_default() as tf_graph:
+            exporter.export_inference_graph(
+                'image_tensor', 
+                config, 
+                checkpoint_path, 
+                output_dir, 
+                input_shape=[batch_size, None, None, 3]
+            )
+
+    # read frozen graph from file
+    frozen_graph = tf.GraphDef()
+    with open(os.path.join(output_dir, FROZEN_GRAPH_NAME), 'rb') as f:
+        frozen_graph.ParseFromString(f.read())
+
+    # apply graph modifications
+    if force_nms_cpu:
+        frozen_graph = f_force_nms_cpu(frozen_graph)
+    if replace_relu6:
+        frozen_graph = f_replace_relu6(frozen_graph)
+    if remove_assert:
+        frozen_graph = f_remove_assert(frozen_graph)
+
+    # get input names
+    # TODO: handle mask_rcnn 
+    input_names = [INPUT_NAME]
+    output_names = [BOXES_NAME, CLASSES_NAME, SCORES_NAME, NUM_DETECTIONS_NAME]
+
+    # remove temporary directory
+    subprocess.call(['rm', '-rf', output_dir])
+
+    return frozen_graph, input_names, output_names
+
+
+def main():
+    # What model to run from - should be the directory name of an exported trained model
+    # Change me to the directory checkpoint files are saved in
+    frozen_graph, input_names, output_names = build_detection_graph(
+        config=CONFIG_FILE,
+        checkpoint=os.path.join(SAVED_MODEL_DIR, MODEL_CHECKPOINT_PREFIX+CHECKPOINT_NUMBER),
+        score_threshold=0.2,
+        batch_size=1
+    )
+    trt_graph = trt.create_inference_graph(
+        input_graph_def=frozen_graph,
+        outputs=output_names,
+        max_batch_size=1,
+        max_workspace_size_bytes=1 << 25,
+        precision_mode='FP32', # TODO - FP16 or INT8 for Jetson
+        minimum_segment_size=50
+    )
+
+    with open(os.path.join(SAVED_MODEL_DIR, TRT_OUTPUT_GRAPH), 'wb') as f:
+        f.write(trt_graph.SerializeToString())
+
+if __name__ == '__main__':
+    main()
---
+++ 2020Game/models/trt_test_detection.py
@@ -0,0 +1,129 @@
+"""
+
+Run detection using optimized trt GPU code.
+
+"""
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.tensorrt as trt
+import cv2
+import os
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+
+def get_frozen_graph(pb_path):
+    """Read Frozen Graph file from disk."""
+    """
+    with tf.gfile.FastGFile(pb_path, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    return graph_def
+    """
+    """Load the TRT graph from the pre-build pb file."""
+    trt_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(pb_path, 'rb') as pf:
+        trt_graph_def.ParseFromString(pf.read())
+
+    """
+    # force CPU device placement for NMS ops
+    for node in trt_graph_def.node:
+        if 'rfcn_' in pb_path and 'SecondStage' in node.name:
+            node.device = '/device:GPU:0'
+        if 'faster_rcnn_' in pb_path and 'SecondStage' in node.name:
+            node.device = '/device:GPU:0'
+        if 'NonMaxSuppression' in node.name:
+            node.device = '/device:CPU:0'
+    """
+    with tf.Graph().as_default() as trt_graph:
+        tf.import_graph_def(trt_graph_def, name='')
+    return trt_graph
+
+# Takes a image, and using the tensorflow session and graph
+# provided, runs inference on the image. This returns a list
+# of detections - each includes the object bounding box, type
+# and confidence
+def run_inference_for_single_image(image, sess):
+    input_names = ['image_tensor']
+    tf_input = sess.graph.get_tensor_by_name(input_names[0] + ':0')
+    tf_scores = sess.graph.get_tensor_by_name('detection_scores:0')
+    tf_boxes = sess.graph.get_tensor_by_name('detection_boxes:0')
+    tf_classes = sess.graph.get_tensor_by_name('detection_classes:0')
+    tf_num_detections = sess.graph.get_tensor_by_name('num_detections:0')
+    scores, boxes, classes, num_detections = sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
+        tf_input: image[None, ...]
+    })
+    output_dict = { }
+    output_dict['detection_boxes'] = boxes[0]  # index by 0 to remove batch dimension
+    output_dict['detection_scores'] = scores[0]
+    output_dict['detection_classes'] = classes[0].astype(np.int64)
+    output_dict['num_detections'] = int(num_detections[0]);
+    return output_dict
+
+def main():
+    # Dir where model.ckpt* files are being generated
+    SAVED_MODEL_DIR='/home/ubuntu/tensorflow_workspace/2020Game/models/tmp4'
+    TRT_OUTPUT_GRAPH = 'trt_graph.pb'
+    # The TensorRT inference graph file downloaded from Colab or your local machine.
+    pb_fname = os.path.join(SAVED_MODEL_DIR, TRT_OUTPUT_GRAPH)
+    trt_graph = get_frozen_graph(pb_fname)
+
+    # Create session and load graph
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    tf_sess = tf.Session(config=tf_config, graph=trt_graph)
+
+    # List of the strings that is used to add correct label for each box.
+    PATH_TO_LABELS = os.path.join('/home/ubuntu/tensorflow_workspace/2020Game/data', '2020Game_label_map.pbtxt')
+    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+    PATH_TO_TEST_IMAGES_DIR = '/home/ubuntu/tensorflow_workspace/2020Game/data/videos'
+
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_Field_Tour_Video_Alliance_Station.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_Field_Tour_Video_Loading_Bay.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_Field_Tour_Video_Power_Port.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_Field_Tour_Video_Rockwell.mp4'))
+    cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_INFINITE_RECHARGE_Field_Drone_Footage_Control_Panel.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_INFINITE_RECHARGE_Field_Drone_Video_Cross_Field_Views.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_INFINITE_RECHARGE_Field_Drone_Video_Cross_Field_Views_1080p.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_INFINITE_RECHARGE_Field_Drone_Video_Field_from_Alliance_Station.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_INFINITE_RECHARGE_Field_Drone_Video_Field_from_Alliance_Station_1080p.mp4'))
+    #cap = cv2.VideoCapture(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_INFINITE_RECHARGE_Field_Drone_Video_Shield_Generator.mp4'))
+
+    # Used to write annotated video (video with bounding boxes and labels) to an output mp4 file
+    #vid_writer = cv2.VideoWriter(os.path.join(PATH_TO_TEST_IMAGES_DIR, '2020_Field_Tour_Video_Power_Port_annotated.avi'), cv2.VideoWriter_fourcc(*"FMP4"), 30., (640,360))
+    while(True):
+      ret, cv_vid_image = cap.read()
+      next_frame = False
+      while (not next_frame):
+        # Vid input is BGR, need to convert to RGB and resize 
+        # to 300x300 to run inference
+        image_rgb = cv2.cvtColor(cv_vid_image, cv2.COLOR_BGR2RGB)
+        image300x300 = cv2.resize(image_rgb, (300,300))
+        output_dict = run_inference_for_single_image(image300x300, tf_sess)
+
+        # output_dictionary will have detection box coordinates, along with the classes
+        # (index of the text labels) and confidence scores for each detection
+        print output_dict
+
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_rgb,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks'),
+            use_normalized_coordinates=True,
+            line_thickness=4,
+            max_boxes_to_draw=50,
+            min_score_thresh=0.35,
+            groundtruth_box_visualization_color='yellow')
+        cv2.imshow('img', cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR))
+        #vid_writer.write(cv2.pyrDown(cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)))
+        key = cv2.waitKey(5) & 0xFF
+        if key == ord("f"):
+          next_frame = True
+        next_frame = True
+
+if __name__ == '__main__':
+    main()
