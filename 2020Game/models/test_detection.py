b0d046c22f57c8ffe1fafecd37c95005cceb4ae5
Kevin Jaget <kjaget@nc.rr.com>
2020-02-17 13:17:33 -0500

    Try to get TF to save the best N eval checkpoints  (#11)
    * Try to get TF to save the best N eval checkpoints instead of the most
    recent ones
    
    * Fix BestEval constructor
    
    * Clean up python imports for test_detection

--- 2020Game/models/test_detection.py
+++ 2020Game/models/test_detection.py
@@ -3,23 +3,15 @@ import cv2
 import glob
 import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
-import tarfile
 import tensorflow as tf
-import zipfile
 
-from distutils.version import StrictVersion
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
 from PIL import Image
 
 # This is needed since the notebook is stored in the object_detection folder.
 #sys.path.append("..")
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
-
 from object_detection.utils import visualization_utils as vis_util
 
 def load_image_into_numpy_array(image):
