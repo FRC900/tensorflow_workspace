e7b18d2b9f9ffccd54b961653adebf3bb9706e94
Team 900 Programmers <programmers@team900.org>
2020-03-02 16:51:09 -0500

    Update training steps to 500000

--- 2020Game/models/train.sh
+++ 2020Game/models/train.sh
@@ -9,7 +9,7 @@
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/targets/x86_64-linux/lib/stubs
 PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_coco.config
 MODEL_DIR=/home/ubuntu/tensorflow_workspace/2020Game/models
-NUM_TRAIN_STEPS=350000
+NUM_TRAIN_STEPS=500000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 python /home/ubuntu/tensorflow_workspace/2020Game/models/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
