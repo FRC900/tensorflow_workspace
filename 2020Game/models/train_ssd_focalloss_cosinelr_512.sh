618d056219d78562b47a6ca8a430aecb7b7efc8a
Kevin Jaget <kjaget@nvidia.com>
2020-08-19 15:03:46 -0400

    Fix typo

--- 2020Game/models/train_ssd_focalloss_cosinelr_512.sh
+++ 2020Game/models/train_ssd_focalloss_cosinelr_512.sh
@@ -7,7 +7,7 @@
 # rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # fi
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/targets/x86_64-linux/lib/stubs
-PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_focalosss_cosinelr_512x512_coco.config
+PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_focalloss_cosinelr_512x512_coco.config
 MODEL_DIR=/home/ubuntu/tensorflow_workspace/2020Game/models
 NUM_TRAIN_STEPS=200000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
