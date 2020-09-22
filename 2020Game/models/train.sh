f3cdcd21e98aee2a72c510aef7fd90039b9b4963
Kevin Jaget <kjaget@nvidia.com>
2020-09-22 19:13:02 -0400

    Updates for py3

--- 2020Game/models/train.sh
+++ 2020Game/models/train.sh
@@ -6,12 +6,12 @@
 # tar -xzf ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # fi
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/targets/x86_64-linux/lib/stubs
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/targets/x86_64-linux/lib/stubs
 PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_coco.config
 MODEL_DIR=/home/ubuntu/tensorflow_workspace/2020Game/models
 NUM_TRAIN_STEPS=500000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python /home/ubuntu/tensorflow_workspace/2020Game/models/model_main.py \
+python3 /home/ubuntu/tensorflow_workspace/2020Game/models/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
