488376c4f928d3936741df410fa2cbefcf66a603
Kevin Jaget <kjaget@nvidia.com>
2020-09-30 11:36:11 -0400

    Merge trt and non-trt detection, they're both the same thing Update train.sh to make it less error prone Updated graph_utils to match tf 1.15 CPU device name

--- 2020Game/models/train.sh
+++ 2020Game/models/train.sh
@@ -7,8 +7,8 @@
 # rm ssd_mobilenet_v2_coco_2018_03_29.tar.gz
 # fi
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/targets/x86_64-linux/lib/stubs
-PIPELINE_CONFIG_PATH=/home/ubuntu/tensorflow_workspace/2020Game/models/model/ssd_mobilenet_v2_coco.config
-MODEL_DIR=/home/ubuntu/tensorflow_workspace/2020Game/models
+MODEL_DIR=/home/ubuntu/tensorflow_workspace/2020Game/models/INSERT_TRAINING_DIR_HERE
+PIPELINE_CONFIG_PATH=$MODEL_DIR/ssd_mobilenet_v2_coco.config
 NUM_TRAIN_STEPS=500000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 python3 /home/ubuntu/tensorflow_workspace/2020Game/models/model_main.py \
