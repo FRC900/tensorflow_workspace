f29653b2fc3890400fc982e57401da90ab67a076
Kevin Jaget <kjaget@nc.rr.com>
2022-01-09 10:05:00 -0500

    Removed 2022 color detection copy - kill it with fire Added WPI field images from kickoff

--- 2022Game/color_detection/simple_model.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:27a5554ee1b88256704556c61f55e61b8f0361601ce35566d6e1c3f8eb73a6d4
-size 6390
--- 2022Game/color_detection/viz_model.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import plot_model
-
-model = load_model('unnormalized_data.h5')
-plot_model(model, to_file='model.png', show_shapes=True)
