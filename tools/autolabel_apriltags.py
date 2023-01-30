d59fd085ba5394f72326ef889938afdfacf77267
Ben Goldberg <67695217+BenBean18@users.noreply.github.com>
2023-01-30 16:12:14 -0500

    apriltag autolabel goes through all xmls in dir

--- tools/autolabel_apriltags.py
+++ tools/autolabel_apriltags.py
@@ -2,37 +2,48 @@ import cv2
 import apriltag # pip install apriltag
 import sys
 from pascal import PascalVOC, PascalObject, BndBox # pip install pascal-voc
+from pathlib import Path
 
 if len(sys.argv) != 2:
-    print("Invalid usage, do `autolabel_apriltags.py file` to use file.jpg and file.xml")
+    print("Invalid usage, do `autolabel_apriltags.py directory` to label all images in a directory")
     sys.exit()
 
-file_stem = sys.argv[1]
+directory = sys.argv[1]
 
-ann = PascalVOC.from_xml(file_stem+".xml")
-objs = ann.objects
-print(objs)
+def chopFilePath(filepath: str):
+    return filepath[filepath.rfind("/")+1:]
 
-img = cv2.imread(file_stem+".jpg", cv2.IMREAD_GRAYSCALE)
-options = apriltag.DetectorOptions(families='tag16h5',
-                                 border=1,
-                                 nthreads=4,
-                                 quad_decimate=0.0,
-                                 quad_blur=0.0,
-                                 refine_edges=True,
-                                 refine_decode=False,
-                                 refine_pose=False,
-                                 debug=False,
-                                 quad_contours=True)
-detector = apriltag.Detector(options=options)
-result = detector.detect(img)
+pathlist = Path(directory).glob('**/*.xml')
+for path in pathlist:
+    # because path is object not string
+    xml = str(path)
 
-good_tags = []
-for tag in result:
-    if tag.hamming == 0:
-        obj = PascalObject("april_tag_"+str(tag.tag_id), "Unspecified", truncated=False, difficult=False, bndbox=BndBox(int(tag.corners[1][0]), int(tag.corners[1][1]), int(tag.corners[3][0]), int(tag.corners[3][1])))
-        good_tags.append(obj)
+    ann = PascalVOC.from_xml(xml)
+    objs = ann.objects
+    #print(objs)
+    imagePath = chopFilePath(ann.filename)
 
-new_objs = objs + good_tags
-ann.objects = new_objs
-ann.save(file_stem+".xml")
+    img = cv2.imread(directory+"/"+imagePath, cv2.IMREAD_GRAYSCALE)
+    options = apriltag.DetectorOptions(families='tag16h5',
+                                    border=1,
+                                    nthreads=4,
+                                    quad_decimate=0.0,
+                                    quad_blur=0.5,
+                                    refine_edges=True,
+                                    refine_decode=True,
+                                    refine_pose=False,
+                                    debug=False,
+                                    quad_contours=True)
+    detector = apriltag.Detector(options=options)
+    result = detector.detect(img)
+
+    good_tags = []
+    for tag in result:
+        #print(tag.hamming)
+        #if tag.hamming == 0:
+            obj = PascalObject("april_tag_"+str(tag.tag_id), "Unspecified", truncated=False, difficult=False, bndbox=BndBox(int(tag.corners[1][0]), int(tag.corners[1][1]), int(tag.corners[3][0]), int(tag.corners[3][1])))
+            good_tags.append(obj)
+
+    new_objs = objs + good_tags
+    ann.objects = new_objs
+    ann.save(xml)
