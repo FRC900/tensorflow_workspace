a91e43450d08888c946ba7f1d34c18d718e6d302
Kevin Jaget <kjaget@nc.rr.com>
2023-02-28 00:24:12 -0500

    more apriltag fixes

--- tools/remove_apriltags.py
+++ tools/remove_apriltags.py
@@ -5,7 +5,8 @@ from pathlib import Path
 print(sys.argv)
 if len(sys.argv) != 3:
     print("Usage: (in current directory)\n\tremove_apriltags.py unduplicate pattern -- removes all duplicate AprilTag bounding boxes and prints out all labels with multiple of the same tag in files matching `pattern`\
-\n\n\tremove_apriltags.py delete pattern -- removes ALL AprilTag detections in files matching `pattern`\n\n\n\tNOTE: pattern must end in .xml Also, * must be escaped as \\*")
+\n\n\tremove_apriltags.py delete pattern -- removes ALL AprilTag detections in files matching `pattern`\n\n\n\tNOTE: pattern must end in .xml Also, * must be escaped as \\*\
+\n\n\tremove_apriltags.py check pattern -- removes ALL AprilTag detections in files matching `pattern`\n\n\n\tNOTE: pattern must end in .xml Also, * must be escaped as \\*")
     sys.exit()
 
 operation = sys.argv[1]
@@ -14,7 +15,7 @@ glob = sys.argv[2]
 def chopFilePath(filepath: str):
     return filepath[filepath.rfind("/")+1:]
 
-pathlist = Path(".").glob(glob)
+pathlist = sorted(Path(".").glob(glob))
 for path in pathlist:
     # because path is object not string
     xml = str(path)
@@ -31,6 +32,7 @@ for path in pathlist:
     for obj in objs:
         if "april_tag" in obj.name:
             if operation == "delete":
+                print(f"DUPLICATE of {obj.name} in {path} with SAME BOUNDING BOXES!!")
                 changed = True
                 continue
             if obj.name in tags.keys():
@@ -44,7 +46,7 @@ for path in pathlist:
         final_objs.append(obj)
 
 
-    if changed:
+    if operation != "check" and changed:
         new_objs = final_objs
         ann.objects = new_objs
         ann.save(xml)
