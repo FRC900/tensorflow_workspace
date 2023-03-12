00732440d63a07b6e59aa5e4ca959cec64c3f1ca
Kevin Jaget <kjaget@nc.rr.com>
2023-03-12 14:03:03 -0400

    Make extract frames tool image creation multi-threaded

--- tools/extract_frames.cpp
+++ tools/extract_frames.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 
+#include <atomic>
 #include <fstream>
 #include <iostream>
 #include <iomanip>
@@ -10,14 +11,17 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <mutex>
 #include <string>
+#include <thread>
 
 //#define DEBUG
 
-static int         g_num_frames = 75;
+static size_t      g_num_frames = 75;
 static std::string g_outputdir  = ".";
+static size_t      g_num_threads = std::thread::hardware_concurrency();
 
-std::string Behead(std::string my_string)
+std::string Behead(const std::string &my_string)
 {
 	const size_t found = my_string.rfind("/");
 
@@ -30,6 +34,7 @@ void usage(char *argv[])
 	std::cout << "usage: " << argv[0] << " [-f frames] [-i files] [-o outdir] filename1 [filename2...]" << std::endl << std::endl;
 	std::cout << "-f          frames is the number of frames grabbed from a video" << std::endl;
 	std::cout << "-o          change output directory from cwd/images to [option]/images" << std::endl;
+	std::cout << "-t          set number of threads used for image generation" << std::endl;
 }
 
 
@@ -53,10 +58,28 @@ std::vector<std::string> Arguments(int argc, char *argv[])
 			{
 				try
 				{
-					g_num_frames = std::stoi(argv[i + 1]);
+					g_num_frames = std::stoul(argv[i + 1]);
 					if (g_num_frames < 1)
 					{
 						std::cout << "Must get at least one frame per file!" << std::endl;
+						break;
+					}
+				}
+				catch (...)
+				{
+					usage(argv);
+					break;
+				}
+				i += 1;
+			}
+			else if (strncmp(argv[i], "-t", 2) == 0)
+			{
+				try
+				{
+					g_num_threads = std::stoul(argv[i + 1]);
+					if (g_num_threads < 1)
+					{
+						std::cout << "Must use at least one thread!" << std::endl;
 						break;
 					}
 				}
@@ -93,7 +116,7 @@ std::vector<std::string> Arguments(int argc, char *argv[])
 
 
 typedef std::pair<double, int> Blur_Entry;
-void readVideoFrames(const std::string &vidName, int &frameCounter, std::vector<Blur_Entry> &lblur)
+void readVideoFrames(const std::string &vidName, size_t &frameCounter, std::vector<Blur_Entry> &lblur)
 {
 	cv::VideoCapture frameVideo(vidName);
 
@@ -133,6 +156,43 @@ void readVideoFrames(const std::string &vidName, int &frameCounter, std::vector<
 }
 
 
+void getFrameFromVideo(cv::VideoCapture &cap, std::mutex &cap_mutex, const int frame_number, cv::Mat &frame)
+{
+	std::lock_guard<std::mutex> l(cap_mutex);
+
+	cap.set(CV_CAP_PROP_POS_FRAMES, frame_number);
+	cap.read(frame);
+}
+
+
+void frameProcessThread(const std::vector<size_t> &frames, const size_t start_frame, const size_t num_frames, const std::string &vid_name, cv::VideoCapture &cap, std::mutex &cap_mutex, std::atomic<size_t> &frame_count)
+{
+	std::vector<int> compression_params;
+	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(9);
+
+	cv::Mat frame;
+
+	for (size_t i = start_frame; (i < (start_frame + num_frames)) && (i < frames.size()); i++)
+	{
+		const size_t frame_num = frames[i];
+		getFrameFromVideo(cap, cap_mutex, frame_num, frame);
+		std::stringstream write_name;
+		write_name << g_outputdir << "/" + Behead(vid_name) << "_" << std::setw(5) << std::setfill('0') << frame_num;
+		write_name << ".png";
+		if (cv::imwrite(write_name.str().c_str(), frame, compression_params) == false)
+		{
+			std::cout << "Error! Could not write file " <<  write_name.str() << std::endl;
+			unlink(write_name.str().c_str());
+		}
+		else
+		{
+			++frame_count;
+		}
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
 	std::vector<std::string> vid_names = Arguments(argc, argv);
@@ -154,28 +214,28 @@ int main(int argc, char *argv[])
 	}
 
 	// Iterate through each input video
-	for (auto vidName = vid_names.cbegin(); vidName != vid_names.cend(); ++vidName)
+	for (const auto &vid_name : vid_names)
 	{
-		std::cout << *vidName << std::endl;
+		std::cout << vid_name << std::endl;
 
 		cv::Mat frame;
-		int frame_counter;
+		size_t frame_counter;
 
 		// Grab an array of frames sorted by how clear they are
 		std::vector<Blur_Entry> lblur;
-		readVideoFrames(*vidName, frame_counter, lblur);
+		readVideoFrames(vid_name, frame_counter, lblur);
 		if (lblur.empty())
 		{
-			std::cout << "Capture not open; invalid video " << *vidName << std::endl;
+			std::cout << "Capture not open; invalid video " << vid_name << std::endl;
 			continue;
 		}
 
-		cv::VideoCapture frame_video(*vidName);
-		int          frame_count = 0;
-		std::vector<bool> frame_used(frame_counter);
-		const int    frame_range = lblur.size()/(g_num_frames * 1.2);      // Try to space frames out by this many unused frames
+		const int frame_range = lblur.size()/(g_num_frames * 1.2);      // Try to space frames out by this many unused frames
 		std::cout << "Creating " << g_num_frames << " images spaced at least " << frame_range << " frames apart" << std::endl;
-		for (auto it = lblur.begin(); (frame_count < g_num_frames) && (it != lblur.end()); ++it)
+
+		std::vector<bool> frame_used(frame_counter);
+		std::vector<size_t> frames_to_process;
+		for (auto it = lblur.begin(); (frames_to_process.size() < g_num_frames) && (it != lblur.end()); ++it)
 		{
 			// Check to see that we haven't used a frame close to this one
 			// already - hopefully this will give some variety in the frames
@@ -195,24 +255,33 @@ int main(int argc, char *argv[])
 				continue;
 			}
 
+			frames_to_process.push_back(this_frame);
 			frame_used[this_frame] = true;
-
-			frame_video.set(CV_CAP_PROP_POS_FRAMES, this_frame);
-			frame_video >> frame;
-
-			std::stringstream write_name;
-			write_name << g_outputdir << "/" + Behead(*vidName) << "_" << std::setw(5) << std::setfill('0') << this_frame;
-			write_name << ".png";
-			std::vector<int> compression_params;
-			compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-			compression_params.push_back(9);
-			if (cv::imwrite(write_name.str().c_str(), frame, compression_params) == false)
-			{
-				std::cout << "Error! Could not write file "<<  write_name.str() << std::endl;
-				unlink(write_name.str().c_str());
-			}
-			frame_count += 1;
 		}
+
+		cv::VideoCapture frame_video(vid_name);
+		if (!frame_video.isOpened())
+		{
+			std::cerr << "Error : couldn't open " << vid_name << " as VideoCapture" << std::endl;
+			continue;
+		}
+		std::mutex frame_video_mutex;
+		std::vector<std::thread> threads;
+		std::atomic<size_t> frame_count{0};
+
+		const size_t frames_per_thread = frames_to_process.size() / g_num_threads + 1;
+		std::cout << "Creating " << g_num_threads << " image creation threads, each processing about " << frames_per_thread << " images" << std::endl;
+
+		for (size_t i = 0; i < g_num_threads; i++)
+		{
+			threads.push_back(std::thread(frameProcessThread, std::ref(frames_to_process), i * frames_per_thread, frames_per_thread, std::ref(vid_name), std::ref(frame_video), std::ref(frame_video_mutex), std::ref(frame_count)));
+		}
+
+		for (auto &t : threads)
+		{
+			t.join();
+		}
+		std::cout << "Succesfully created " << frame_count << " output frame images" << std::endl;
 	}
 	return 0;
 }
